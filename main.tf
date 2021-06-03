2bf1c9216b02e53ebf52daa1bf8ed5f7534336be
Mike Augustine <practicalint@users.noreply.github.com>
2021-06-03 10:21:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "mike-a-consul"
     }
   }
   required_providers {
